366d402272bec4f814d430f899f110cdbdebfaeb
Msplahari156 <priyankka.156@gmail.com>
2025-07-30 21:39:42 +0530

    first commit

---
+++ src/browserlaunching/ChromeLaunch2.java
@@ -0,0 +1,19 @@
+package browserlaunching;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ChromeLaunch2 {
+
+	public static void main(String[] args) {
+		//approach 2
+		//using selenium 3.141.59 +WebDriver Manager
+		//got to below path adn downlaod fat jar and add build path - classpath - add jar apply close
+		//https://github.com/bonigarcia/webdrivermanager/releases/tag/webdrivermanager-6.2.0
+		WebDriverManager.chromedriver().setup();
+		new ChromeDriver();
+
+	}
+
+}
