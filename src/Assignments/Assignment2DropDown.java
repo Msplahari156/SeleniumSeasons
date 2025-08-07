a8c5e76d11eeed59bf36865c1391151b6b50ddb5
Msplahari156 <priyankka.156@gmail.com>
2025-08-07 21:43:10 +0530

    asssignment2

---
+++ src/Assignments/Assignment2DropDown.java
@@ -0,0 +1,34 @@
+package Assignments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Assignment2DropDown {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.linkText("Create new account")).click();
+		Thread.sleep(3000);
+		
+		new Select(driver.findElement(By.id("day"))).selectByIndex(3);
+		new Select(driver.findElement(By.id("month"))).selectByIndex(6);
+		new Select(driver.findElement(By.id("year"))).selectByIndex(29);
+		
+		/*new Select(driver.findElement(By.id("day"))).selectByValue("4");
+		new Select(driver.findElement(By.id("month"))).selectByValue("7");
+		new Select(driver.findElement(By.id("year"))).selectByValue("1996");*/
+		
+		
+		
+		/*new Select(driver.findElement(By.id("day"))).selectByVisibleText("4");
+		new Select(driver.findElement(By.id("month"))).selectByVisibleText("Jul");
+		new Select(driver.findElement(By.id("year"))).selectByVisibleText("1996"); */
+		Thread.sleep(4000);
+        driver.quit();
+	}
+
+}
